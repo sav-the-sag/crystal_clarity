0b34d45c297e836e62e4ec378068693dd7aabe0a
estan <elise.stankus@gmail.com>
2024-03-05 10:54:39 -0800

    added User, Affirmation, and Auth types to typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,22 @@
 const typeDefs = `
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        bio: String
+        savedAffirmations: [Affirmation]
+    }
 
+    type Affirmation {
+        _id: ID
+        message: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
 `;
 
 module.exports = typeDefs;
