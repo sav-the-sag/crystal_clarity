b51841b6c77f161b0d72047dbfc6d58a9a310577
estan <elise.stankus@gmail.com>
2024-03-05 11:36:10 -0800

    added bio to addUser mutation, added updateBio mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -29,10 +29,11 @@ const typeDefs = `
 
     type Mutation {
         login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!, bio: String): Auth
         removeUser: User
         saveAffirmation(affirmationData: AffirmationInput!): User
         removeAffirmation(message: String!): User
+        updateBio(bio: String!): User
     }
 
 `;
