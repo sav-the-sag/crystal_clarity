6803bb1d217fe5ac86488045b1d44ccf4bb925cd
estan <elise.stankus@gmail.com>
2024-03-04 21:12:02 -0800

    created new instance of user schema, added username and email fields

--- server/models/User.js
+++ server/models/User.js
@@ -1,3 +1,18 @@
 //dependencies
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/.+@.+\..+/, 'Must match an email address!'],
+    }
+})
