9f6c5d48784d9a59d5d693a8cd583add7b7af184
estan <elise.stankus@gmail.com>
2024-03-04 21:24:00 -0800

    created new instance of affirmation schema, exported schema

--- server/models/Affirmation.js
+++ server/models/Affirmation.js
@@ -1,3 +1,11 @@
 // dependencies
 const { Schema } = require('mongoose');
 
+const affirmationSchema = new Schema({
+    message: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = affirmationSchema;
