0abc3de8d27ee8ed8215352cb23624078c3d3e26
savan <savannahwilliams243@gmail.com>
2024-03-06 15:58:54 -0800

    used me in query to get logged in user

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,15 @@
 const { User } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+      // retrieve the logged in user from the context and find the user details in the database
+      me: async (parent, args, context) => {
+        if (context.user) {
+          return User.findOne({ _id: context.user._id });
+        }
+      }
+    },
+}
+
+module.exports = resolvers;
