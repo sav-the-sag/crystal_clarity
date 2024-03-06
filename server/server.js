dafbe2dc006219bdcc488d1ab845f7748ec3db5a
estan <elise.stankus@gmail.com>
2024-03-06 14:07:11 -0800

    added middleware to new instance of apollo server

--- server/server.js
+++ server/server.js
@@ -19,5 +19,21 @@ const server = new ApolloServer({
 // new instance of Apollo server with graphql schema
 const startApolloServer = async () => {
     await server.start();
+
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
     
+    // client-side requests starting with /graphql are handled by the apollo server
+    app.use('/graphql', expressMiddleware(server, {
+        context: authMiddleware
+    }));
+
+    // in production, client-side side bundle is delivered from dist/ folder
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+
+        app.get('*', (req,res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
+    }
 }
