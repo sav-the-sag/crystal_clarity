2f45ce06f85b95d859cd5a00157610bee929293e
estan <elise.stankus@gmail.com>
2024-03-06 13:48:06 -0800

    required dependencies

---
+++ server/server.js
@@ -0,0 +1,10 @@
+// dependencies
+const express = require('express');
+const path = require('path');
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
+
