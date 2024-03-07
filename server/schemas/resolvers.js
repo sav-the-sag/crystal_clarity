c6679a0cb9ec783cf65e6776a11c43216dcfaf5e
savan <savannahwilliams243@gmail.com>
2024-03-06 16:02:24 -0800

    if user is not logged in, the AuthenticationError will happen

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -9,7 +9,8 @@ const resolvers = {
         if (context.user) {
           return User.findOne({ _id: context.user._id });
         }
-      }
+        throw new AuthenticationError('You need to be logged in!');
+      },
     },
 }
 
