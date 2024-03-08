ac1b1e3d93722746f81d4755d9ff68d0c02b8b80
Siennaxstarr <siennastarrv@gmail.com>
2024-03-07 19:10:41 -0800

    added login function to login jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,10 @@
+export default function Login() {
+    return (
+      <div>
+        <h2>Login Page</h2>
+        <p>
+         Login in to generate your daily affirmation! 
+        </p>
+      </div>
+    );
+  }
