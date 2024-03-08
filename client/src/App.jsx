fd2ef21cef664bfe43f33c1f60e4cae381ceceed
Siennaxstarr <siennastarrv@gmail.com>
2024-03-07 19:38:35 -0800

    added contents to app.jsx and imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,45 +1,22 @@
-import { useState } from 'react';
-import './App.css';
+import { Outlet } from 'react-router-dom';
 
-// Add these require statements at the top
-const Globe = require('./pages/Globe');
-const crystalImage = require('./assets/crystal.png');
+import Header from './components/Header/Header';
+
+import Footer from './components/Footer/Footer'
+
+import './App.css'
 
 function App() {
-    const [affirmation, setAffirmation] = useState('');
 
-    // Function to generate affirmation
-    const generateAffirmation = () => {
-        // Logic to generate affirmation
-        const affirmations = [
-            "You are capable of achieving great things.",
-            "You are worthy of love and respect.",
-            "You possess the strength to overcome challenges.",
-            "You radiate positivity and kindness.",
-            "You attract abundance and success into your life."
-        ];
-        const randomIndex = Math.floor(Math.random() * affirmations.length);
-        setAffirmation(affirmations[randomIndex]);
-    };
-
-    return (
-        <div className="app-container">
-            <header>
-                <img src={crystalImage} alt="Crystal Image" />
-                <h1>Affirmation Crystal</h1>
-            </header>
-            <main>
-                <div className="crystal-globe">
-                    <Globe />
-                </div>
-                <div className="affirmation">{affirmation}</div>
-                <button id="generate-button" onClick={generateAffirmation}>Generate Affirmation</button>
-            </main>
-            <footer>
-                <p>&copy; 2024 Affirmation Crystal Globe</p>
-            </footer>
-        </div>
-    );
+  return (
+    <div>
+     < Header />
+     <main>
+      < Outlet />
+     </main>
+     < Footer />
+    </div>
+  )
 }
 
-export default App;
+export default App
