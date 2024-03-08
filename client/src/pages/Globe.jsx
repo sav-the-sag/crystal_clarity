96a9d978bf47ee39a417b13bb770a7fa32d49f7c
Kendrink <you@example.com>
2024-03-07 21:21:04 -0800

    started globe content

--- client/src/pages/Globe.jsx
+++ client/src/pages/Globe.jsx
@@ -1,11 +1,27 @@
-import './Globe.css'; // Import the CSS file for styling the globe
-import crystalImage from '../assets/crystal.png';
+import "./Globe.css"; // Import the CSS file for styling the globe
+const crystalImage = "../assets/crystal.png"
 
 function Globe() {
   return (
-    <div className="globe-container">
-      <div className="globe">
-        <img src={crystalImage} alt="Crystal" className="crystal-image" />
+    <div>
+      <h2>Affirmation Crystal</h2>
+      <div className="globe-container">
+        <div className="globe">
+          <img src={crystalImage} alt="Crystal" className="crystal-image" />
+        </div>
+      </div>
+      <div className="app-container">
+        
+        <main>
+          
+          <div className="affirmation">{affirmation}</div>
+          <button id="generate-button" onClick={generateAffirmation}>
+            Generate Affirmation
+          </button>
+        </main>
+        <footer>
+          <p>&copy; 2024 Affirmation Crystal Clarity</p>
+        </footer>
       </div>
     </div>
   );
