136aa776aeb84de06c8324d88d8fb6b1e7fe0338
Siennaxstarr <siennastarrv@gmail.com>
2024-03-07 19:14:43 -0800

    Added Signup function to sign up jsx

---
+++ client/src/pages/Signup.jsx
@@ -0,0 +1,10 @@
+export default function Signup() {
+    return (
+      <div>
+        <h2>Signup Page</h2>
+        <p>
+         Signup to access your daily affirmation! 
+        </p>
+      </div>
+    );
+  }
