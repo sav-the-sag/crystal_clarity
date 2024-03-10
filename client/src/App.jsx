0fca27ab02f64367a5493c8aee4b17bc9ab95228
estan <elise.stankus@gmail.com>
2024-03-10 16:21:47 -0700

    fixed file paths for header and footer components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer'
+import Header from './components/Header';
+import Footer from './components/Footer'
 import './App.css'
 // allows interaction with graphQL api on frontend //
 import {
