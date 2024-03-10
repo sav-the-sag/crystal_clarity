c60872441ad83c61774c0c7b9590d50273455913
savan <savannahwilliams243@gmail.com>
2024-03-10 13:28:53 -0700

    created RemoveUser component

---
+++ client/src/components/RemoveUser.jsx
@@ -0,0 +1,73 @@
+// imports
+import { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from '@apollo/client';
+import { REMOVE_USER } from '../utils/mutations';
+
+const RemoveUser = () => {
+    const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+    const [removeUser, { error }] = useMutation(REMOVE_USER)
+
+    const handleRemoveUser = async (event) => {
+        event.preventDefault();
+        // get token
+        const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+        if (!token) {
+            return false;
+        }
+        try {
+            const { data } = await removeUser({
+                variables: { ...userFormData }
+            });
+        } catch (err) {
+            console.error(err);
+        }
+
+        setUserFormData({
+            email: '',
+            password: '',
+        });
+    };
+
+    return (
+        <>
+            <h4> Are you sure you want to delete your profile?</h4>
+            <br />
+            <h6> If so, please enter your email and password. </h6>
+            <Form onSubmit={handleRemoveUser}>
+                <Form.Group>
+                    <Form.Label htmlFor='email'>Email</Form.Label>
+                    <Form.Control
+                        type='text'
+                        placeholder='Your email'
+                        name='email'
+                        value={userFormData.email}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label htmlFor='password'>Password</Form.Label>
+                    <Form.Control
+                        type='password'
+                        placeholder='Your password'
+                        name='password'
+                        value={userFormData.password}
+                        required
+                    />
+                    <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+                </Form.Group>
+                <Button
+                    disabled={!(userFormData.email && userFormData.password)}
+                    type='submit'
+                    variant='danger'>
+                    Submit
+                </Button>
+            </Form>
+        </>
+    );
+}
+
+export default RemoveUser;
