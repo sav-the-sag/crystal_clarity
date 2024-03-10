5ae2c8375a104aa63af23754a19ded85f11beed6
estan <elise.stankus@gmail.com>
2024-03-10 16:16:34 -0700

    deconstructed save_affirmation and get_affirmation

--- client/src/components/AffirmationModal.jsx
+++ client/src/components/AffirmationModal.jsx
@@ -1,7 +1,7 @@
 // import dependencies
 import { useQuery, useMutation } from '@apollo/client';
-import SAVE_AFFIRMATION from "../utils/mutations";
-import GET_AFFIRMATION from "../utils/queries";
+import { SAVE_AFFIRMATION } from "../utils/mutations";
+import { GET_AFFIRMATION } from "../utils/queries";
 
 const AffirmationModal = () => {
     const { loading, data } = useQuery(GET_AFFIRMATION);
