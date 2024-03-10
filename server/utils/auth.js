34e88637823e1d5ed33869c7d490aeff5381a845
savan <savannahwilliams243@gmail.com>
2024-03-10 13:00:12 -0700

    added GraphQL authentication error and authMiddleware

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -6,6 +6,35 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  AuthenticationError: new GraphQLError('Oops! Failed authentication :(', {
+    extensions: {
+      code: 'UNAUTHENTICATED',
+    },
+  }),
+  authMiddleware: function ({ req }) {
+    // allows token to be sent via req.body, req.query, or headers
+    let token = req.body.token || req.query.token || req.headers.authorization;
+
+    // We split the token string into an array and return actual token
+    if (req.headers.authorization) {
+      token = token.split(' ').pop().trim();
+    }
+
+    if (!token) {
+      return req;
+    }
+
+    // if token can be verified, add the decoded user's data to the request for resolvers
+    try {
+      const { data } = jwt.verify(token, secret, { maxAge: expiration });
+      req.user = data;
+    } catch {
+      console.log('Invalid token');
+    }
+
+    // return the request object for context in resolvers
+    return req;
+  },
     signToken: function ({ username, email, _id }) {
       const payload = { username, email, _id };
   
