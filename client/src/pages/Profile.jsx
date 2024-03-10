fc32ec3d966ab3051c5d2737b465a8b3f3c489fe
estan <elise.stankus@gmail.com>
2024-03-09 19:19:30 -0800

    imported dependencies

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,3 +1,10 @@
+import { useQuery, useMutation } from '@apollo/client';
+
+import GET_ME from '../utils/queries'
+import { REMOVE_AFFIRMATION, UPDATE_INT, REMOVE_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
+
 export default function Profile() {
     return (
       <div>
