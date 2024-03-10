09b68778cb079c6af132e7682bc34997b1d2beca
estan <elise.stankus@gmail.com>
2024-03-09 19:46:28 -0800

    added useQuery for get me query, added return statement content, nested affirmationsList component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,17 +1,35 @@
 import { useQuery, useMutation } from '@apollo/client';
 
 import GET_ME from '../utils/queries'
-import { REMOVE_AFFIRMATION, UPDATE_INT, REMOVE_USER } from '../utils/mutations';
+import { REMOVE_USER } from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
 export default function Profile() {
-    return (
-      <div>
-        <h2>Profile Page</h2>
-        <p>
-         Profile page! 
-        </p>
-      </div>
-    );
+
+  const { loading, data } = useQuery(GET_ME);
+
+  const user = data?.me || {};
+
+  if (loading) {
+    return <h2>Loading...</h2>;
   }
+  return (
+    <div>
+      <h2>Welcome {user.username}!</h2>
+      <div>
+      {user.savedAffirmations?.length
+            ? `Your affirmations:`
+            : 'You have no saved affirmations!'}
+      </div>
+      <div>
+      {user.savedAffirmations?.length > 0 && (
+        <AffirmationsList
+          savedAffirmations={user.savedAffirmations}
+          isLoggedInUser={true}
+        />
+      )}
+      </div>
+    </div>
+  );
+}
