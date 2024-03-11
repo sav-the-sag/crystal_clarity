b63323a18d479235e3946195a6a6e85c60b697b1
estan <elise.stankus@gmail.com>
2024-03-11 02:20:26 -0700

    updated get_affirmation query -- removed data types

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,8 +2,8 @@ import {gql} from '@apollo/client';
 export const GET_AFFIRMATION =gql`
 query affirmation {
 affirmation {
-    affirmationId: ID
-    message: String
+    affirmationId
+    message
 }
 }`
 
