5e6b13dfc840d1cadb1c09ca5b4bbe5f161c640e
estan <elise.stankus@gmail.com>
2024-03-11 14:18:16 -0700

    removed email and password from removeUser mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -31,7 +31,7 @@ const typeDefs = `
     type Mutation {
         login(email: String!, password: String!): Auth
         addUser(username: String!, email: String!, password: String!): Auth
-        removeUser(email: String!, password: String!): User
+        removeUser: User
         saveAffirmation(affirmationData: AffirmationInput!): User
         removeAffirmation(affirmationId: ID!): User
         updateInt(intention: String!): User
