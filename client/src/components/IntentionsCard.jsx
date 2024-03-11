b23c0a3910049ecdbac638e80fa8ef298fd220c0
estan <elise.stankus@gmail.com>
2024-03-11 11:03:17 -0700

    added handleinputchange, exported card component, fixed file path for get_me query

--- client/src/components/IntentionsCard.jsx
+++ client/src/components/IntentionsCard.jsx
@@ -1,23 +1,33 @@
 import { useMutation } from '@apollo/client';
 import { UPDATE_INT } from '../utils/mutations';
-import { GET_ME } from '../../utils/queries';
+import { GET_ME } from '../utils/queries';
 import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 
 const IntentionsCard = ({ intention }) => {
-    const [intention, setIntention] = useState('');
-    const [updateInt, { error }] = useMutation
+    const [intentionData, setIntentionData] = useState({intention: ''});
+    const [updateInt, { data, error }] = useMutation
         (UPDATE_INT, {
             refetchQueries: [
                 GET_ME,
                 'me'
             ]
         });
+
+    const handleInputChange = (event) => {
+        const { name, value } = event.target;
+        setIntentionData({
+            ...intentionData,
+            [name]: value
+        })
+    }
     const handleUpdateInt = async (event) => {
         event.preventDefault()
+        console.log(intentionData)
+
         try {
             const { data } = await updateInt({
-                variables: { intention },
+                variables: { ...intentionData },
             });
             setIntention('')
         } catch (err) {
@@ -37,13 +47,13 @@ const IntentionsCard = ({ intention }) => {
                     <Form.Control
                         type='text'
                         placeholder='Your Intention'
-                        name='Intention'
-                        value={intention}
+                        name='intention'
+                        value={intentionData.intention}
                         required
                     />
                 </Form.Group>
                 <Button
-                    disabled={!(intention)}
+                    disabled={!(intentionData.intention)}
                     type='submit'
                 >
                     Set Intention
@@ -52,3 +62,4 @@ const IntentionsCard = ({ intention }) => {
     )
 }
 
+export default IntentionsCard;
