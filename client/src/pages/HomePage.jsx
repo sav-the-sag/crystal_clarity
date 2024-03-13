21ecd7f8fc0622e714b770802fa7cbc94e592e6f
Siennaxstarr <siennastarrv@gmail.com>
2024-03-13 10:10:28 -0800

    updated homepage content

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,9 +1,11 @@
 export default function Home() {
   return (
     <div>
-      <h2>Home!</h2>
+      <h2>Crystal Clarity</h2>
       <p>
-       nice home
+      Welcome to Crystal Clarity! This is a premier destination for cultivating positivity and motivating affirmations! Our app offers a mystical insights though a Magic Crystal Ball.
+      Users can retrieve affirmations from a seeded library, create personalized profiles to curate their favorite affirmations, and enjoy the transformative power of positive thinking. With intuitive features for updating profiles and managing collections, Crystal Clarity empowers individuals to embark on a journey of self-discovery and enlightenment!
+                                                                              ~Enjoy~ 
       </p>
     </div>
   );
